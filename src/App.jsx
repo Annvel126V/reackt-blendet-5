e692bf073fc3184d0f35f4710a4552f2f2218b80
Anna Velychko <162054651+Annvel126V@users.noreply.github.com>
2024-10-13 14:27:21 +0200

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,42 @@
-import { Heading } from 'components';
+import { lazy, useEffect } from 'react';
+import { getUserInfo } from './service/opencagedataApi';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { Header } from './components';
+const Home = lazy(() => import('pages/Home'));
+const Rates = lazy(() => import('pages/Rates'));
 
 export const App = () => {
-  return <Heading title="Just do it!" />;
+  useEffect(() => {
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    };
+
+    function success(pos) {
+      const crd = pos.coords;
+      getUserInfo(pos.coords);
+      console.log('Your current position is:');
+      console.log(`Latitude : ${crd.latitude}`);
+      console.log(`Longitude: ${crd.longitude}`);
+      console.log(`More or less ${crd.accuracy} meters.`);
+    }
+
+    function error(err) {
+      console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+
+    navigator.geolocation.getCurrentPosition(success, error, options);
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/rates" element={<Rates />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </>
+  );
 };

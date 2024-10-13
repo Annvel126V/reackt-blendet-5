5640ba60c178ce6bdc445bf730ca640591660183
Anna Velychko <162054651+Annvel126V@users.noreply.github.com>
2024-10-13 15:58:22 +0200

    add exchangeform

--- src/components/ExchangeForm/ExchangeForm.jsx
+++ src/components/ExchangeForm/ExchangeForm.jsx
@@ -2,13 +2,32 @@ import { RiExchangeDollarFill } from 'react-icons/ri';
 import styles from './ExchangeForm.module.css';
 
 export const ExchangeForm = () => {
+  const onSubmit = e => {
+    e.preventDefault();
+    const { value } = e.target.elements.currency;
+    console.log(value);
+    console.log({
+      to: 'UAH',
+      from: 'USD',
+      amount: 15,
+    });
+  };
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={onSubmit}>
       <button className={styles.button} type="submit">
         <RiExchangeDollarFill className={styles.icon} />
       </button>
 
-      <input title="Request format 15 USD in UAH" className={styles.input} />
+      <input
+        title="Request format 15 USD in UAH"
+        pattern="^\d+(\.\d{1,2})?\s[a-zA-Z]{3}\sin\s[a-zA-Z]{3}$"
+        placeholder="15 USD in UAH"
+        name="currency"
+        type="text"
+        required
+        className={styles.input}
+      />
     </form>
   );
 };
